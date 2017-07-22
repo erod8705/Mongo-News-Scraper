b232fbdae34b32d4f1e554d21b430a3df64f9b7a
Erica Rodriguez <erica.rodriguez.mail@gmail.com>
2017-07-22 10:32:24 -0500

    changed port number

--- server.js
+++ server.js
@@ -150,6 +150,5 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+app.listen(process.env.PORT || 3000, function () {
+  console.log("app run on default or 3000!")});
